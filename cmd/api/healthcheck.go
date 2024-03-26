d848fda4a6306094e02b8464b95b75d3aa72f855
Effiong Bassey <effiong.bassey@arm.com.ng>
2024-03-26 01:05:42 +0100

    Added JSON writer helper function

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "environment: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "An error occured while processing your request", http.StatusInternalServerError)
+	}
 
 }
